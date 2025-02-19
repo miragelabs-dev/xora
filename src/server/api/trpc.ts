feec0b7996644d7740ea5d623d82897ec7cf00ae
Omer Alpi <omeralpi@icloud.com>
2025-02-19 23:11:15 +0300

    feat: integrate tRPC with React Query and Zod (#7)

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,26 @@
+import { initTRPC } from "@trpc/server";
+import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+export const createTRPCContext = async (
+  opts: Omit<CreateNextContextOptions, "info">
+) => opts;
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const createTRPCRouter = t.router;
+
+export const publicProcedure = t.procedure;
