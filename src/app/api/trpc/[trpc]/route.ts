c92a5fafc95e38d3a48a5993c9f9f08d008adb33
Omer Alpi <omeralpi@icloud.com>
2025-02-22 23:46:37 +0300

    refactor: simplify tRPC route handler for edge runtime

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,25 +1,17 @@
 import { appRouter } from "@/server/api/routers"
 import { createTRPCContext } from "@/server/api/trpc"
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
-import { NextApiRequest, NextApiResponse } from "next"
 
 export const config = {
   runtime: "edge",
 }
 
-const handler = async (
-  req: NextApiRequest & Request,
-  res: Request & NextApiResponse
-) =>
+const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: async () =>
-      await createTRPCContext({
-        req,
-        res,
-      }),
+    createContext: () => createTRPCContext(),
   })
 
 export { handler as GET, handler as POST }
