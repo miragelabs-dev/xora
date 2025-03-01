1c39c9cc82959ce8c035d53db1f6805f04c8ec97
Omer Alpi <omeralpi@icloud.com>
2025-03-01 11:28:50 +0300

    feat: add metadata configuration for improved SEO and page titles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { CryptoProvider } from "@/contexts/crypto-context";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { TRPCProvider } from './trpc-provider';
@@ -9,6 +10,15 @@ const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Xora',
+    default: 'Xora',
+  },
+  description: 'Welcome to our Decentralized Twitter Clone built with Chopin Framework and Celestia!',
+  metadataBase: new URL('https://xora.social'),
+};
 
 export default async function RootLayout({
   children,
